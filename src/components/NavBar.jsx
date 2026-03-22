4f1a23779c5110b86d0a7569eb7c7f7768931645
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2026-03-22 22:42:18 +0100

    add a navigation bar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,38 @@
+
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+
+const NavBar = () =>{
+
+    const [Connected, setConnected] = useState(false)
+
+    return (
+        <nav>
+            <div className="container" >
+                <Link className="navbar-brand" >BOOKHOUSE</Link>
+                {/* boutton d'ouvertir et fermeture des liens */}
+                <button className="navbar-toggler" >
+                    <span>menu</span>
+                </button>
+                <div className="collapse navbar-collapse" >
+                    {/* lien de navigation */}
+                    <ul className="navbar-nav" >
+                        <li className="nav-item" > <Link className='nav-link' href="/book" >Nos livres</Link> </li>
+                        <li className="nav-item" > <Link className='nav-link' href="/book" >Favoris</Link> </li>
+                        <li className="nav-item" > <Link className='nav-link' href="/book" >Nous contacter</Link> </li>
+                    </ul>
+                    {/* barre de recherche */}
+                    <form>
+                        <input type="search" className="form-control" placeholder="Rechercher un livre" />
+                        <button type="submit" className="btn btn-outline-dark" ></button>
+                    </form>
+                    {Connected ? <Link>profil </Link>  : <Link>login</Link> }
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar;
+

87732bbbf3c2633a11045b4119f707c90469d872
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2026-05-17 14:41:08 +0200

    create a get data in dashboard but fail now use a context instead

--- src/components/forms/LoginForm.jsx
+++ src/components/forms/LoginForm.jsx
@@ -1,22 +1,43 @@
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import api from "../"
+import api from "../../utils/axiosConfig"
 import "./style/auth.css";
 
 function LoginForm() {
+    const navigate = useNavigate();
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
 
-    const handleSubmit = (e) =>{
+    const handleSubmit = async (e) =>{
         e.preventDefault();
+        setError('');
         console.log("Données capturées :", { email, password});
+
+        const credentials = { email, password };
+
+        try {
+            const response = await api.post('/user/login', credentials);
+
+            if(response.status === 200){
+                console.log("Connexion réussi !", response.data);
+
+                navigate('/dashboard');
+            }
+
+        } catch (error) {
+            console.log("Erreur de connexion :", error.response?.data);
+            setError(error.response?.data?.message || "Email ou mot de passe incorrect.")
+        }
+
     }
 
     return (
         <div className="auth-container">
             <div className="auth-card">
                 <h2>Bon retour !</h2>
+                {error && <div className="error-message" style={{color: '#ef4444', marginBottom: '15px', fontWeight: 'bold'}}>{error}</div>}
                 <p className="auth-subtitle">Connectez-vous pour gérer vos annonces</p>
                 
                 <form className="auth-form" onSubmit={handleSubmit} >
--- src/pages/UserProfil.jsx
+++ src/pages/UserProfil.jsx
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import Sidebar from '../components/layout/Sidebar';
 import "./styles/dashboard.css";
+import DefaultUser from "../assets/default-profile.png";
+import api from '../utils/axiosConfig.js';
 
 function UserProfile() {
-    // Pour l'instant, on simule l'utilisateur. 
-    // Plus tard, on récupérera ces infos depuis le Backend.
-    const user = {
-        username: "Utilisateur Test",
-        email: "test@example.com",
-        profilePicture: "https://via.placeholder.com/150"
-    };
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState('');
+
+    useEffect(() => {
+        const fetchUserProfile =async () => {
+            try {
+                const response = await api.get('/user/profile');
+                console.log(response);
+                if(response.status === 200){
+                    setUser(response.data);
+                }
+            } catch (error) {
+                console.error("Erreur lors de la récupération du profil : ", error);
+                setError("Impossible de charger les informations du profil.");
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchUserProfile();
+    }, [] );
+
+    if(loading){
+        return <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center',height: '100vh'}} >
+            Chargement du profile ...
+        </div>
+    }
+
+    if(error){
+        return <div style={{ color: 'red', textAlign: 'center', marginTop: '50px' }} >
+            {error}
+        </div>
+    }
 
     return (
         <div className="dashboard-layout">
@@ -22,10 +50,10 @@ function UserProfile() {
                 </div>
 
                 <div className="profile-card">
-                    <img src={user.profilePicture} alt="Profil" className="profile-img" />
+                    <img src={user?.profilePicture || DefaultUser} alt="Profil" className="profile-img" />
                     <div className="profile-info">
-                        <h3>{user.username}</h3>
-                        <p>{user.email}</p>
+                        <h3>{user?.username}</h3>
+                        <p>{user?.email}</p>
                         <button className="edit-btn">Modifier le profil</button>
                     </div>
                 </div>
@@ -33,11 +61,11 @@ function UserProfile() {
                 {/* Ici tu pourras rajouter les sections futures */}
                 <div className="dashboard-stats">
                     <div className="stat-box">
-                        <h4>0</h4>
+                        <h4>{user?.booksCount || 0}</h4>
                         <p>Livres postés</p>
                     </div>
                     <div className="stat-box">
-                        <h4>0</h4>
+                        <h4>{user?.favoritesCount || 0}</h4>
                         <p>Favoris</p>
                     </div>
                 </div>
